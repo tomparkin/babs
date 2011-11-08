9ea813de6601ace5d19f9a0f2e6dec1fa5d35525
Tom Parkin <tom.parkin@gmail.com>
2011-11-08 11:33:39 +0000

    autobuilder: push logts() into liblog

--- liblog.sh
+++ liblog.sh
@@ -3,6 +3,7 @@
 # Bash log library
 #
 log() { echo "$@"; }
+logts() { log "[$(date)] $@"; }
 dbg() { test -n "$DEBUG" && log "$@" 1>&2 || true; }
 err() { log "ERROR: $@" 1>&2; false; }
 die() { err "$@"; exit 1; }
