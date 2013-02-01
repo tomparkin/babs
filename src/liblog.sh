086b84e52b9dd870311c692e56b3c3e292c2e2b8
Tom Parkin <tom.parkin@gmail.com>
2013-02-01 23:04:49 +0000

    Add syslog support

--- src/liblog.sh
+++ src/liblog.sh
@@ -2,8 +2,8 @@
 #
 # Bash log library
 #
-log() { echo "$@"; }
-logts() { log "[$(date)] $@"; }
-dbg() { test -n "$DEBUG" && log "$@" 1>&2 || true; }
-err() { log "ERROR: $@" 1>&2; false; }
+log() { echo "$@"; logger -p user.notice -t babs "$@"; }
+logts() { log "[$(date)] $@"; logger -p user.notice -t babs "$@"; }
+dbg() { test -n "$DEBUG" && log "$@" 1>&2 && logger -p user.debug -t babs "$@" || true; }
+err() { log "ERROR: $@" 1>&2; logger -p user.err -t babs "$@"; false; }
 die() { err "$@"; exit 1; }
